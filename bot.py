b11eeb130900602135a19280f37d9b027dce39d9
maitopapahu <owaisbabakatv@gmail.com>
2025-04-14 03:28:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 from pyrogram import Client
 import os
 
-TOKEN = os.environ.get("TOKEN", "")
+TOKEN = os.environ.get("TOKEN", "7529913637:AAFr-E6m5HRQLwhCRGUZBhT9pUfzcwRnG4Q")
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "29696002"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "b0402db44bb51470d0c50d3be950781b")
 
 if __name__ == "__main__" :
     plugins = dict(
